ff3cb9c148391f324476943e99658d7aaa735d29
Brian Lee <brian112358@gmail.com>
2018-01-06 21:06:24 -0800

    Initial Let's Split keymap.

---
+++ keyboards/lets_split/keymaps/brian/rules.mk
@@ -0,0 +1,7 @@
+# TAP_DANCE_ENABLE = yes
+# Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+RGBLIGHT_ENABLE = yes
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
